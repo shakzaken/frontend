14d298a2bfa43b7e7b5256ddaa1490abb9738aee
יקיר זקן <yakir@yqyrs-MacBook-Pro.local>
2022-06-27 14:31:06 +0300

    small change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          shak attack
+          shak attack 2
         </p>
       </header>
     </div>
