e61fed09bdc8a3decabfa0c053c9503c33ed73c8
יקיר זקן <yakir@yqyrs-MacBook-Pro.local>
2022-06-27 14:34:49 +0300

    small yml change2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          shak attack 2
+          shak attack 3
         </p>
       </header>
     </div>
