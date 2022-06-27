8772a963180a7a9af0a28adfb8cc0f61c94a9380
יקיר זקן <yakir@yqyrs-MacBook-Pro.local>
2022-06-27 12:16:33 +0300

    change test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const number = 5+3*10;
+
+  expect(number).toBe(35);
 });
